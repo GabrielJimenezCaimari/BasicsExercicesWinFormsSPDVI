1b7f6b4b1a10c9b7c56de6acd43ab3ce163f28ed
unknown <gabrieljimenez@paucasesnovescifp.cat>
2020-10-15 23:09:09 +0200

    Exercice6 Es parecido al Exercice5 pero en este no hay String[]y solo usa una String color para guardar el radiobutton elegido

---
+++ Exercice6/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Exercice6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.Mostrar = new System.Windows.Forms.TextBox();
+            this.Option1 = new System.Windows.Forms.RadioButton();
+            this.Option2 = new System.Windows.Forms.RadioButton();
+            this.Option3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(52, 244);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(89, 43);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // Mostrar
+            // 
+            this.Mostrar.Location = new System.Drawing.Point(52, 325);
+            this.Mostrar.Name = "Mostrar";
+            this.Mostrar.Size = new System.Drawing.Size(705, 26);
+            this.Mostrar.TabIndex = 1;
+            // 
+            // Option1
+            // 
+            this.Option1.AutoSize = true;
+            this.Option1.Location = new System.Drawing.Point(263, 61);
+            this.Option1.Name = "Option1";
+            this.Option1.Size = new System.Drawing.Size(64, 24);
+            this.Option1.TabIndex = 2;
+            this.Option1.TabStop = true;
+            this.Option1.Text = "Red";
+            this.Option1.UseVisualStyleBackColor = true;
+            this.Option1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Option2
+            // 
+            this.Option2.AutoSize = true;
+            this.Option2.Location = new System.Drawing.Point(263, 113);
+            this.Option2.Name = "Option2";
+            this.Option2.Size = new System.Drawing.Size(66, 24);
+            this.Option2.TabIndex = 3;
+            this.Option2.TabStop = true;
+            this.Option2.Text = "Blue";
+            this.Option2.UseVisualStyleBackColor = true;
+            // 
+            // Option3
+            // 
+            this.Option3.AutoSize = true;
+            this.Option3.Location = new System.Drawing.Point(263, 166);
+            this.Option3.Name = "Option3";
+            this.Option3.Size = new System.Drawing.Size(75, 24);
+            this.Option3.TabIndex = 4;
+            this.Option3.TabStop = true;
+            this.Option3.Text = "White";
+            this.Option3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Option3);
+            this.Controls.Add(this.Option2);
+            this.Controls.Add(this.Option1);
+            this.Controls.Add(this.Mostrar);
+            this.Controls.Add(this.buttonOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.TextBox Mostrar;
+        private System.Windows.Forms.RadioButton Option1;
+        private System.Windows.Forms.RadioButton Option2;
+        private System.Windows.Forms.RadioButton Option3;
+    }
+}
+
---
+++ Exercice6/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            String color = "";
+            if(Option1.Checked)
+            {
+                color = Option1.Text;
+            }
+
+            if (Option2.Checked)
+            {
+                color = Option2.Text;
+            }
+
+            if (Option3.Checked)
+            {
+                color = Option3.Text;
+            }
+
+            Mostrar.Text = "Choosen colour: " + color;
+        }
+    }
+}
