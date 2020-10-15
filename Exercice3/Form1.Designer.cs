9498b0d651e609c47b1cb6495475fb4c5d6fd3b4
unknown <gabrieljimenez@paucasesnovescifp.cat>
2020-10-15 19:31:23 +0200

    Exercice 3 una suma con 3 textbox y dos botones 1 para realizar e la suma y el otro que hace un bucle para poner todos los textbox a 0

---
+++ Exercice3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Exercice3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Operand2 = new System.Windows.Forms.TextBox();
+            this.Operand1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonRestart);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(229, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonsum);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Operand1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Operand2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(330, 328);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(117, 26);
+            this.Result.TabIndex = 8;
+            // 
+            // Operand2
+            // 
+            this.Operand2.Location = new System.Drawing.Point(330, 205);
+            this.Operand2.Name = "Operand2";
+            this.Operand2.Size = new System.Drawing.Size(117, 26);
+            this.Operand2.TabIndex = 9;
+            // 
+            // Operand1
+            // 
+            this.Operand1.Location = new System.Drawing.Point(330, 109);
+            this.Operand1.Name = "Operand1";
+            this.Operand1.Size = new System.Drawing.Size(117, 26);
+            this.Operand1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Operand1);
+            this.Controls.Add(this.Operand2);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.TextBox Operand2;
+        private System.Windows.Forms.TextBox Operand1;
+    }
+}
+
---
+++ Exercice3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRestart(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "0";
+                    this.Operand2.Focus();
+                }
+            }
+        }
+        private void buttonsum(object sender, EventArgs e)
+        {
+            Result.Text = (int.Parse(Operand1.Text) + int.Parse(Operand2.Text)).ToString();
+        }
+    }
+}
